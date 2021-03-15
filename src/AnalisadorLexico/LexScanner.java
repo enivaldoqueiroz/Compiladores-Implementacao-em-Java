9daa08c09e3ba2094aa8072fef4b53a22dd87438
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-03-15 01:39:31 -0300

    Implementação do Token e alguns ajustes

--- src/AnalisadorLexico/LexScanner.java
+++ src/AnalisadorLexico/LexScanner.java
@@ -6,23 +6,73 @@ import java.nio.file.Paths;
 
 public class LexScanner {
 	
-	private char[] content;
-	private int estado;
+	private char[] 	content;
+	private int 	estado;
+	private int		posicao;
 	
 	public LexScanner(String filename) {
 		try {
 			
 			String txtConteudo;
-			txtConteudo = new String(Files.readAllBytes(Paths.get(filename),StandardCharsets.UTF_8));
+			txtConteudo = new String(Files.readAllBytes(Paths.get(filename)),StandardCharsets.UTF_8);
 			System.out.println("DEBUG ----------");
 			System.out.println(txtConteudo);
 			System.out.println("----------------");
 			content = txtConteudo.toCharArray();
+			posicao = 0;
 			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 				
+	}
+	
+	//Metodo principal da classe LexScanner
+	public Token nextToken() {
+		char currentChar;
+		if (isEOF()) {
+			return null;
+		}
+		estado = 0;
+		while (true) {
+			currentChar = nextChar();
+			
+			switch(estado) {
+			case 0:
+				if(isChar(currentChar)) {
+					estado = 1;
+					
+				}
+				else if (isDigit(currentChar)) {
+					estado = 3;
+					
+				}
+				else if (isSpace(currentChar)) {
+					estado = 0;
+					
+				}
+				else if(isOperator(currentChar)) {
+					estado = 5;
+				}
+				else {
+					throw new RuntimeException("Unrecognized SYMBOL");
+				}
+				break;
+			case 1:
+				if(isChar(currentChar) || isDigit(currentChar)) {
+					estado = 1;
+				}
+				else {
+					estado = 2;
+				}
+				break;
+			case 2:
+				Token token = new Token();
+				token.setType(Token.TK_IDENTIFIER);
+				return token;
+					
+			}
+		}
 	}
 	
 	private boolean isDigit(char c) {
@@ -40,5 +90,18 @@ public class LexScanner {
 	private boolean isSpace(char c) {
 		return c == ' ' || c == '\t' || c == '\n' || c == '\r';
 	}
+	
+	private char nextChar() {
+		return content[posicao++];
+	}
+	
+	private boolean isEOF() {
+		return posicao == content.length;
+	}
+	
+	private void back() {
+		posicao--;
+	}
+	
 
 }
--- src/AnalisadorLexico/MainCLass.java
+++ src/AnalisadorLexico/MainCLass.java
@@ -2,6 +2,15 @@ package AnalisadorLexico;
 
 public class MainCLass {
 	public static void main(String[] args) {
+		LexScanner sc = new LexScanner("input.eni");
+		Token token = null;
 		
+		do {
+			token = sc.nextToken();
+			if (token != null) {
+				System.out.println(token);
+			}
+			
+		} while (token != null);
 	}
 }
